60182cabf4fbe90c01a151974ef9c1ef070ee2d7
BenaSPACE <BenaSPACE@gmail.com>
2023-07-17 16:17:38 -0400

    Added Javascript

---
+++ fill-in.js
@@ -0,0 +1,43 @@
+document.getElementById("submit").onclick = function () {
+    var word = document.getElementById('noun7Box').value;
+    document.getElementById("noun7").innerHTML = word;
+    var word = document.getElementById('noun6Box').value;
+    document.getElementById("noun6").innerHTML = word;
+    var word = document.getElementById('noun5Box').value;
+    document.getElementById("noun5").innerHTML = word;
+    var word = document.getElementById('noun4Box').value;
+    document.getElementById("noun4").innerHTML = word;
+    var word = document.getElementById('noun3Box').value;
+    document.getElementById("noun3").innerHTML = word;
+    var word = document.getElementById('noun2Box').value;
+    document.getElementById("noun2").innerHTML = word;
+    var word = document.getElementById('noun1Box').value;
+    document.getElementById("noun1").innerHTML = word;
+
+    var word = document.getElementById('verb4Box').value;
+    document.getElementById("verb4").innerHTML = word;
+    var word = document.getElementById('verb3Box').value;
+    document.getElementById("verb3").innerHTML = word;
+    var word = document.getElementById('verb2Box').value;
+    document.getElementById("verb2").innerHTML = word;
+    var word = document.getElementById('verb1Box').value;
+    document.getElementById("verb1").innerHTML = word;
+
+    var word = document.getElementById('adj3Box').value;
+    document.getElementById("adj3").innerHTML = word;
+    var word = document.getElementById('adj2Box').value;
+    document.getElementById("adj2").innerHTML = word;
+    var word = document.getElementById('adj1Box').value;
+    document.getElementById("adj1").innerHTML = word;
+
+    var word = document.getElementById('gameBox').value;
+    document.getElementById("game").innerHTML = word;
+    var word = document.getElementById('plantBox').value;
+    document.getElementById("plant").innerHTML = word;
+    var word = document.getElementById('bodypartBox').value;
+    document.getElementById("bodypart").innerHTML = word;
+    var word = document.getElementById('placeBox').value;
+    document.getElementById("place").innerHTML = word;
+    var word = document.getElementById('numberBox').value;
+    document.getElementById("number").innerHTML = word;
+}
